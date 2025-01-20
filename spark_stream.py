5bfae44ce4f796ad12b15bcd99ccd733c5010964
Harrabi Fadwa <142733588+FadwaHarrabi@users.noreply.github.com>
2025-01-20 22:21:29 +0100

    add dependecies and run dag to execute the pipeline and store the data in the cassandra database after submit spark job

--- spark_stream.py
+++ spark_stream.py
@@ -1,38 +1,21 @@
 import logging
+
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType
 
-# Configure logging for better debugging and status tracking
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-
 
 def create_keyspace(session):
-    """
-    Creates a keyspace in Cassandra if it doesn't already exist.
-    a keyspace is an important entity in Cassandra that dictates how data 
-    is distributed and replicated, and it provides a logical 
-    grouping for tables within the database.
-    """
-
-
-    """
-    In Cassandra, the session.execute() method is used to run CQL (Cassandra Query Language)
-    """
     session.execute("""
         CREATE KEYSPACE IF NOT EXISTS spark_streams
         WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
     """)
+
     print("Keyspace created successfully!")
-    logging.info("Keyspace created successfully!")
+
 
 def create_table(session):
-    """
-    Creates a table in Cassandra to store user data if it doesn't already exist.
-    """
     session.execute("""
     CREATE TABLE IF NOT EXISTS spark_streams.created_users (
         id UUID PRIMARY KEY,
@@ -47,16 +30,13 @@ def create_table(session):
         phone TEXT,
         picture TEXT);
     """)
+
     print("Table created successfully!")
-    logging.info("Table created successfully!")
+
 
 def insert_data(session, **kwargs):
-    """
-    Inserts user data into the Cassandra table.
-    """
-    logging.info("Inserting data...")
+    print("inserting data...")
 
-    # Extracting values from the keyword arguments
     user_id = kwargs.get('id')
     first_name = kwargs.get('first_name')
     last_name = kwargs.get('last_name')
@@ -71,7 +51,6 @@ def insert_data(session, **kwargs):
     picture = kwargs.get('picture')
 
     try:
-        # Insert data into the table
         session.execute("""
             INSERT INTO spark_streams.created_users(id, first_name, last_name, gender, address, 
                 post_code, email, username, dob, registered_date, phone, picture)
@@ -81,15 +60,12 @@ def insert_data(session, **kwargs):
         logging.info(f"Data inserted for {first_name} {last_name}")
 
     except Exception as e:
-        logging.error(f"Could not insert data due to {e}")
+        logging.error(f'could not insert data due to {e}')
+
 
 def create_spark_connection():
-    """
-    Creates a Spark session configured with necessary packages for Kafka and Cassandra.
-    """
-
-    """ use mvnrepository to get connectors """
     s_conn = None
+
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
@@ -97,17 +73,16 @@ def create_spark_connection():
                                            "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
+
         s_conn.sparkContext.setLogLevel("ERROR")
         logging.info("Spark connection created successfully!")
     except Exception as e:
-        logging.error(f"Couldn't create the Spark session due to exception: {e}")
+        logging.error(f"Couldn't create the spark session due to exception {e}")
 
     return s_conn
 
+
 def connect_to_kafka(spark_conn):
-    """
-    Connects to a Kafka topic and reads data as a Spark DataFrame.
-    """
     spark_df = None
     try:
         spark_df = spark_conn.readStream \
@@ -116,31 +91,27 @@ def connect_to_kafka(spark_conn):
             .option('subscribe', 'users_created') \
             .option('startingOffsets', 'earliest') \
             .load()
-        logging.info("Kafka DataFrame created successfully")
+        logging.info("kafka dataframe created successfully")
     except Exception as e:
-        logging.warning(f"Kafka DataFrame could not be created because: {e}")
+        logging.warning(f"kafka dataframe could not be created because: {e}")
 
     return spark_df
 
+
 def create_cassandra_connection():
-    """
-    Establishes a connection to the Cassandra cluster.
-    """
     try:
-        # Connect to Cassandra cluster
+        # connecting to the cassandra cluster
         cluster = Cluster(['localhost'])
+
         cas_session = cluster.connect()
-        logging.info("Cassandra connection created successfully!")
+
         return cas_session
     except Exception as e:
-        logging.error(f"Could not create Cassandra connection due to: {e}")
+        logging.error(f"Could not create cassandra connection due to {e}")
         return None
 
+
 def create_selection_df_from_kafka(spark_df):
-    """
-    Extracts and parses Kafka data into a structured Spark DataFrame.
-    """
-    # Define the schema for the Kafka data
     schema = StructType([
         StructField("id", StringType(), False),
         StructField("first_name", StringType(), False),
@@ -155,41 +126,34 @@ def create_selection_df_from_kafka(spark_df):
         StructField("picture", StringType(), False)
     ])
 
-    # Parse Kafka messages and extract data
     sel = spark_df.selectExpr("CAST(value AS STRING)") \
         .select(from_json(col('value'), schema).alias('data')).select("data.*")
-    logging.info("Data selection DataFrame created successfully!")
+    print(sel)
+
     return sel
 
+
 if __name__ == "__main__":
-    # Create Spark connection
+    # create spark connection
     spark_conn = create_spark_connection()
 
     if spark_conn is not None:
-        # Connect to Kafka with Spark connection
+        # connect to kafka with spark connection
         spark_df = connect_to_kafka(spark_conn)
-        if spark_df is not None:
-            selection_df = create_selection_df_from_kafka(spark_df)
-            # Connect to Cassandra
-            session = create_cassandra_connection()
-            if session is not None:
-                # Set up Cassandra keyspace and table
-                create_keyspace(session)
-                create_table(session)
+        selection_df = create_selection_df_from_kafka(spark_df)
+        session = create_cassandra_connection()
 
-                logging.info("Streaming is being started...")
+        if session is not None:
+            create_keyspace(session)
+            create_table(session)
 
-                # Start streaming data to Cassandra
-                streaming_query = (selection_df.writeStream
-                                   .format("org.apache.spark.sql.cassandra")
-                                   .option('checkpointLocation', '/tmp/checkpoint')
-                                   .option('keyspace', 'spark_streams')
-                                   .option('table', 'created_users')
-                                   .start())
+            logging.info("Streaming is being started...")
 
-                # Await termination of the stream
-                streaming_query.awaitTermination()
+            streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra")
+                               .option('checkpointLocation', '/tmp/checkpoint')
+                               .option('keyspace', 'spark_streams')
+                               .option('table', 'created_users')
+                               .start())
 
-
+            streaming_query.awaitTermination()
 #docker exec -it cassandra cqlsh -u cassandra -p cassandra localhost 9042
-#his command connects you to the Cassandra container and opens the CQL shell, allowing you to run queries on the Cassandra database using the default username and password (cassandra/cassandra) on port 9042pip install cassandra-driver
